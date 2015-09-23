a78f15303cd684e8c36054de58b0a234f3b8408f
Yufi <yfhj1990@hotmail.com>
2015-09-23 17:35:43 +0800

    color picker logic done.

--- color-picker/draw.el
+++ color-picker/draw.el
@@ -1,31 +1,90 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;
-(point)
+(require 'ov)
+(require 's)
 
 
-(let ((s "Javascript"))
-      (put-text-property 0 (length s) 'face '(:background "cyan") s)
-      (insert s))
+(defvar color-picker-window-width 36)
+(defvar color-picker-buffer-name "*Color Picker*")
+(setq current-buffer (current-buffer))
 
-(make-face 'temp-face)
-(set-face-background 'temp-face "cyan")
+(defun insert-text-center (width text)
+  "Insert text and center align it."
+  (let* ((text-len (string-width text))
+	 (space-width (/ (- width text-len) 2)))
+    (insert (make-string space-width ?\s))
+    (insert text)))
 
-(make-face 'test-face)
-(set-face-background 'test-face "#d32f2f")
+(defun insert-left-space (width len)
+  "Keep text center."
+  (insert (make-string (/ (- width len) 2) ?\s)))
+(defun insert-color-block (width len color)
+  "Insert color block."
+  ;(insert-left-space width len)
+  (ov-set (ov-insert (make-string 7 ?\s)) 'face '(:background "#d32f2f")))
 
-(read-color "#229057053")
+(defun button-click-handle (button)
 
-(color-defined-p "mdl-red-700")
+  (let* ((start (button-start button))
+	 (split-start (- start 14))
+	 (value (s-trim (buffer-substring split-start start))))
 
-(window-body-width)
+    (set-buffer current-buffer)
+    (delete-other-windows)
+    (kill-buffer color-picker-buffer-name)
+    (insert value)
+    )p
+  )
+
+(save-current-buffer
+  (when (get-buffer color-picker-buffer-name)
+    (kill-buffer color-picker-buffer-name))
+  (generate-new-buffer color-picker-buffer-name)
+  (set-buffer (get-buffer-create color-picker-buffer-name))
+  (font-lock-mode nil)
 
 
-(put-text-property 1 83 'face 'temp-face)
+  (let* ((current-window (selected-window))
+	 (current-window-width (window-body-width))
+	 (window (split-window-right (- current-window-width color-picker-window-width)))
+	 (window-width (+ 2 (window-body-width window)))
+	 (title "Material Design Colors")
+	 (title-len (string-width title)))
 
-(put-text-property 83 (1+ (* 82 2)) 'face 'test-face)
+    (set-window-buffer window color-picker-buffer-name)
+    (select-window window)
+
+    (newline)
+    (insert-text-center window-width title)
+    (newline 4)
+    (insert "1.")
+    (insert "Red")
+    (newline 2)
+    (insert-left-space window-width 35)
+    (ov-set (ov-insert (make-string 7 ?\s)) 'face '(:background "#d32f2f"))
+    (newline)
+    (insert-left-space window-width 35)
+    (ov-set (ov-insert (make-string 7 ?\s)) 'face '(:background "#d32f2f"))
+    (insert "   rgb: (255,255,255) [pick]")
+    (newline)
+    (insert-left-space window-width 35)
+    (ov-set (ov-insert (make-string 7 ?\s)) 'face '(:background "#d32f2f"))
+    (insert "   rgb: (255,255,255) [pick]")
+    (newline)
+    (insert-left-space window-width 35)
+    (ov-set (ov-insert (make-string 7 ?\s)) 'face '(:background "#d32f2f"))
+    (insert "   rgb: (255,255,255) ")
+    (insert-button "[pick]" 'action 'button-click-handle)
+    (newline)
+    (insert-left-space window-width 35)
+    (ov-set (ov-insert (make-string 7 ?\s)) 'face '(:background "#d32f2f"))
+
+    
+    
+    )
+  (beginning-of-buffer)
+  (read-only-mode t)
+
+  )
 
 
-(buffer-string)
 
-
+;; insert center
