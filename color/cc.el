c503f51e1dc79b3f0e6959b2c7969b962ba61888
Yufi <yfhj1990@hotmail.com>
2015-09-28 19:39:21 +0800

    add darken.

--- color/cc.el
+++ color/cc.el
@@ -15,20 +15,17 @@
 (require 'dash-functional)
 (require 's)
 
-(defvar cc-re-hex "\\(#\\)\\([0-9a-fA-F]\\{2\\}\\)\\([0-9a-fA-F]\\{2\\}\\)\\([0-9a-fA-F]\\{2\\}\\)"
+(defconst cc-re-hex "\\(#\\)\\([0-9a-fA-F]\\{2\\}\\)\\([0-9a-fA-F]\\{2\\}\\)\\([0-9a-fA-F]\\{2\\}\\)"
   "Regex for hex color.")
-(defvar cc-re-shex "\\(#\\)\\([0-9a-fA-F]\\{1\\}\\)\\([0-9a-fA-F]\\{1\\}\\)\\([0-9a-fA-F]\\{1\\}\\)"
+(defconst cc-re-shex "\\(#\\)\\([0-9a-fA-F]\\{1\\}\\)\\([0-9a-fA-F]\\{1\\}\\)\\([0-9a-fA-F]\\{1\\}\\)"
   "Regex for short hex color.")
-(defvar cc-re-rgb "\\(rgb\\)\(\\([0-9]\\{1,3\\}\\),\s?\\([0-9]\\{1,3\\}\\),\s?\\([0-9]\\{1,3\\}\\)\s?\)"
+(defconst cc-re-rgb "\\(rgb\\)\(\\([0-9]\\{1,3\\}\\),\s?\\([0-9]\\{1,3\\}\\),\s?\\([0-9]\\{1,3\\}\\)\s?\)"
   "Regex for rgb color.")
-(defvar cc-re-hsl "\\(hsl\\)\(\\([0-9]\\{1,3\\}\\),\s?\\([0-9]\\{1,3\\}\\)\%,\s?\\([0-9]\\{1,3\\}\\)\%\s?\)"
+(defconst cc-re-hsl "\\(hsl\\)\(\\([0-9]\\{1,3\\}\\),\s?\\([0-9]\\{1,3\\}\\)\%,\s?\\([0-9]\\{1,3\\}\\)\%\s?\)"
   "Regex for hsl color.")
 
-;;
-;; Convert with rgb color and hsl color.
-;;
 (defsubst cc-h-rgb (m1 m2 h0)
-  "Compute middle value."
+  "Compute hue value."
   (-let* ((h1 (when (< h0 0) (1+ h0)))
 	  (h2 (when (> h0 1) (1- h0)))
 	  (h (--first (not (eq it nil)) (list h1 h2 h0))))
@@ -41,7 +38,7 @@
       (list t m1))))))
 
 (defsubst cc-rgb-hsl (rgb)
-  "Convert rgb to hsl."
+  "Convert rgb to hsl color."
   ;; http://en.wikipedia.org/wiki/HSL_and_HSV#Conversion_from_RGB_to_HSL_or_HSV
   (-let* ((rgbl (--map (/ it 255.0) rgb))
 	  (ma (-max rgbl))
@@ -60,7 +57,7 @@
     (list h s l)))
 
 (defsubst cc-hsl-rgb (hsl)
-  "Convert hsl to rgb."
+  "Convert hsl to rgb color."
   ;; http://www.w3.org/TR/css3-color/#hsl-color
   (-let* (((h s l) hsl)
 	  (m2 (if (<= l 0.5) (* l (1+ s)) (+ l s (- (* l s)))))
@@ -70,11 +67,8 @@
 	  (b (cc-h-rgb m1 m2 (- h (/ 1.0 3)))))
     (--map (round (* it 255)) (list r g b))))
 
-;;
-;; Convert color to mate date
-;;
 (defsubst cc-hex-mate (c)
-  "Check hex format color."
+  "Convert hex format to mate."
   (-if-let* ((li (s-match cc-re-hex c))
 	     ((_ _ r g b) li)
 	     (rgbl (--map (string-to-number it 16) (list r g b)))
@@ -82,7 +76,7 @@
       (-concat rgbl hsll)))
 
 (defsubst cc-shex-mate (c)
-  "Check short hex format color."
+  "Convert short hex format to mate."
   (-if-let* ((li (s-match cc-re-shex c))
 	     ((_ _ r g b) li)
 	     (rgbl (--map
@@ -95,7 +89,7 @@
       (-concat rgbl hsll)))
 
 (defsubst cc-rgb-mate (c)
-  "Check rgb format color."
+  "Convert rgb format to mate."
   (-if-let* ((li (s-match cc-re-rgb c))
 	     ((_ _ r g b) li)
 	     (rgbl (-map 'string-to-number (list r g b)))
@@ -103,7 +97,7 @@
       (-concat rgbl hsll)))
 
 (defsubst cc-hsl-mate (c)
-  "Check hsl format color."
+  "Convert hsl format to mate."
   (-if-let* ((li (s-match cc-re-hsl c))
 	     ((_ _ h0 s0 l0) li)
 	     ((h1 s1 l1) (-map 'string-to-number (list h0 s0 l0)))
@@ -114,9 +108,8 @@
 	     (rgbl (cc-hsl-rgb hsll)))
       (-concat rgbl hsll)))
 
-
 (defsubst cc-mate (c)
-  "Convert color value to color mate date."
+  "Convert Any color value to color mate date."
   (--first
    (not (eq it nil))
    (--map (funcall it c)
@@ -126,14 +119,11 @@
 	    'cc-rgb-mate
 	    'cc-hsl-mate))))
 
-;;
-;; Output color
-;;
-(defun cc-round-float (n)
+(defsubst cc-round-float (n)
   "Round float to per cent."
   (round (* n 100)))
 
-(defun cc-hex-fmt (m)
+(defsubst cc-hex-fmt (m)
   "Output hex color."
   (-let (((r g b) m))
     (format "#%x%x%x" r g b)))
@@ -143,7 +133,7 @@
   (-let (((r g b) m))
     (format "rgb(%d, %d, %d)" r g b)))
 
-(defun cc-hsl-fmt (m)
+(defsubst cc-hsl-fmt (m)
   "Output hsl color."
   (-let (((_ _ _ h s l) m))
     (format "hsl(%d, %d%%, %d%%)"
@@ -152,7 +142,7 @@
 	    (cc-round-float l))))
 
 (defun cc-color (c fmt)
-  "Convert color hex|rgb|hsl"
+  "Format hex|rgb|hsl color."
   (interactive)
   (funcall
    (plist-get
@@ -160,30 +150,29 @@
     (intern fmt))
    (cc-mate c)))
 
-;;
-;; Color value change loop.
-;; 
-(defvar cc-trun-list '("hex" "rgb" "hsl") "List for color format change.")
-(defvar cc-trun-dir "next" "Direction of color change trun.")
+(defvar cc-turn-list '("hex" "rgb" "hsl") "List for color format change.")
+(defvar cc-turn-dir "next" "Direction of color change turn.")
+(defvar cc-darken-value 1 "Darken value per times.")
+(defvar cc-lighten-value 1 "Lighten value per times.")
 
 (defun cc-hex-flag (c)
-  "Make hex color flag."
+  "Test hex color flag."
   (if (s-match cc-re-hex c) "hex" nil))
 
 (defun cc-shex-flag (c)
-  "Make short hexc color flag."
+  "Test short hexc color flag."
   (if (s-match cc-re-shex c) "hex" nil))
 
 (defun cc-rgb-flag (c)
-  "Make rgb color flag."
+  "Test rgb color flag."
   (if (s-match cc-re-rgb c) "rgb" nil))
 
 (defun cc-hsl-flag (c)
-  "Make hsl color flag."
+  "Test hsl color flag."
   (if (s-match cc-re-hsl c) "hsl" nil))
 
 (defun cc-flag (c)
-  "Make color flag."
+  "Test color flag."
   (--first
    (not (eq it nil))
    (--map (funcall it c)
@@ -193,44 +182,32 @@
 	   'cc-rgb-flag
 	   'cc-hsl-flag))))
 
-(defun cc-next (flag trun-list)
-  "Next flag form trun list."
+(defun cc-next (flag turn-list)
+  "Next flag form turn list."
   (-let* ((current flag)
-	  (len (length trun-list))
-	  (idx (-elem-index current trun-list))
+	  (len (length turn-list))
+	  (idx (-elem-index current turn-list))
 	  (cidx (if (>= (1+ idx) len) 0 (1+ idx))))
-    (nth cidx trun-list)))
+    (nth cidx turn-list)))
 
-(defun cc-prev (flag trun-list)
-  "Prev flag form trun list."
+(defun cc-prev (flag turn-list)
+  "Prev flag form turn list."
   (-let* ((current flag)
-	  (len (length trun-list))
-	  (idx (-elem-index current trun-list))
+	  (len (length turn-list))
+	  (idx (-elem-index current turn-list))
 	  (cidx (if (< (1- idx) 0) (1- len) (1- idx))))
-    (nth cidx trun-list)))
+    (nth cidx turn-list)))
 
-(defun cc-trun (c)
+(defun cc-turn (c)
   "Change color format."
   (interactive)
   (-let* ((flag (cc-flag c))
-	  (dir (if  (string= cc-trun-dir "prev") 'cc-prev 'cc-next))
-	  (flag-trun (funcall dir flag cc-trun-list)))
-    
-    (cc-color c flag-trun)
-    ))
-
-
-;; func
-(defun cc-darken (c)
-  ""
-  )
-(defun cc-lighten (c)
-  ""
-  )
-
+	  (dir (if  (string= cc-turn-dir "prev") 'cc-prev 'cc-next))
+	  (flag-turn (funcall dir flag cc-turn-list)))
+    (cc-color c flag-turn)))
 
 (defun cc-find (regexp)
-  ""
+  "Check current regexp."
   (when (re-search-forward regexp (line-end-position) t) regexp))
 
 (defun cc-match ()
@@ -244,8 +221,30 @@
 	   cc-re-rgb
 	   cc-re-hsl))))
 
+(defun cc-darken-mate (c)
+  "draken current color"
+  (-let* ((mate (cc-mate c))
+	  ((r g b h s l0) mate)
+	  (d (/ cc-darken-value 100.0))
+	  (l1 (- l0 d))
+	  (l (if (<= l1 0) 0 l1))
+	  (hsll (list h s l))
+	  (rgbl (cc-hsl-rgb hsll)))
+    (-concat rgbl hsll)))
+
+(defun cc-lighten-mate (c)
+  "draken current color"
+  (-let* ((mate (cc-mate c))
+	  ((r g b h s l0) mate)
+	  (d (/ cc-lighten-value 100.0))
+	  (l1 (+ l0 d))
+	  (l (if (>= l1 1) 0 l1))
+	  (hsll (list h s l))
+	  (rgbl (cc-hsl-rgb hsll)))
+    (-concat rgbl hsll)))
+
 ;;;###autoload
-(defun cc-change ()
+(defun cc-change-turn ()
   "mark color change"
   (interactive)
   (goto-char (line-beginning-position))
@@ -256,8 +255,46 @@
 	  (ed (re-search-forward re (line-end-position) t))
 	  (color (buffer-substring-no-properties be ed)))
     (delete-region be ed)
-    (insert (cc-trun color))))
+    (insert (cc-turn color))))
+
+
+;;;###autoload
+(defun cc-change-darken ()
+  "mark color change"
+  (interactive)
+  (goto-char (line-beginning-position))
+  ;; TODO search string
+
+  (-let* ((re (cc-match))
+	  (be (re-search-backward re (line-beginning-position) t))
+	  (ed (re-search-forward re (line-end-position) t))
+	  (color (buffer-substring-no-properties be ed))
+	  (flag (cc-flag color)))
+    (delete-region be ed)
+    (insert (funcall
+	     (plist-get
+	      '(hex cc-hex-fmt rgb cc-rgb-fmt hsl cc-hsl-fmt)
+	      (intern flag))
+	     (cc-darken-mate color)))))
+
+;;;###autoload
+(defun cc-change-lighten ()
+  "mark color change"
+  (interactive)
+  (goto-char (line-beginning-position))
+  ;; TODO search string
+
+  (-let* ((re (cc-match))
+	  (be (re-search-backward re (line-beginning-position) t))
+	  (ed (re-search-forward re (line-end-position) t))
+	  (color (buffer-substring-no-properties be ed))
+	  (flag (cc-flag color)))
+    (delete-region be ed)
+    (insert (funcall
+	     (plist-get
+	      '(hex cc-hex-fmt rgb cc-rgb-fmt hsl cc-hsl-fmt)
+	      (intern flag))
+	     (cc-lighten-mate color)))))
 
 (provide 'cc)
-
 ;;; c.el end here.
